8e5e61b228ae4135bbd12c00f89e295f19f8f142
francesjanetalabong-bit <francesjanetalabong@gmail.com>
2025-10-23 00:51:59 +0800

    Enhance anime list input and exit functionality
    Refactor anime list program for improved user experience and clarity.

--- CODE_CHALLENGE15.py
+++ CODE_CHALLENGE15.py
@@ -1,19 +1,23 @@
-#Anime list
-#Using list and while loop
+# Anime list
+# Using list and while loop
+
 name = input("Enter your name before you start listing your Anime Title :) ")
-print ("Welcome to the Anime Title List",  name )
+print("Welcome to the Anime Title List,", name)
 print("Anime Title List")
-print("*please enter  that's all, to end the list:")
-anime = [] #empty list
-entry = True   
 
-while entry == True:
-    num = input("Put a Title of the Anime: ")
-    print("Anime Added to the List")
-    anime.append(num) #new  title will be put in the bottom of the list
-    if num == "that's all": #ending
-        print("All done")
-        anime.pop() #the word exit will be remove
+anime_list = []  # empty list
+
+while True:
+    anime = input("Enter the Title of the Anime (or type 'exit' to finish): ")
+
+    if anime.lower() == 'exit':  # check first before adding
+        print("\nAll done! You have exited the anime entry program.")
+        print("Your anime list includes:")
+        for a in anime_list:
+            print(f"- {a}")
         break
+    else:
+        anime_list.append(anime)
+        print("Anime added to the list!")
 
-print(f"All Anime in your list {anime}") #will print all the anime tha the user listed 
+print(f"\nAll Anime in your list: {anime_list}")
