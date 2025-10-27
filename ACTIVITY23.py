ddd576f80ab76045836b3d3a8369006ea3fae4fc
francesjanetalabong-bit <francesjanetalabong@gmail.com>
2025-10-27 23:05:37 +0800

    Add files via upload

---
+++ ACTIVITY23.py
@@ -0,0 +1,13 @@
+# months = ( "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", )
+# # print("months")
+# months.append ("AUGUST")
+
+full_name = "FRANCES JANE A. TALABONG" 
+name = list (full_name)
+print(name)
+
+for i in full_name[0 : 24]:
+  print (i)
+  
+name.reverse()
+print(name)
