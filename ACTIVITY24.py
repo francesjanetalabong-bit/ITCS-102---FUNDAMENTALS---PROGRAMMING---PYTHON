ea3cfc31f503a1ff70bb3d8623a0670df481af0b
francesjanetalabong-bit <francesjanetalabong@gmail.com>
2025-11-07 11:28:45 +0800

    Add files via upload

---
+++ ACTIVITY24.py
@@ -0,0 +1,20 @@
+def welcoming(name): 
+    print( f"Welcome too the Programming World. {name}!")
+
+
+def sum(f):
+    sum = 21
+    for i in range (f, 0, -1):
+        sum = sum + i 
+        print(i)
+    print(f"The sum of number from f to 1 should be : {sum}")
+
+sum(4)
+sum(21)
+
+
+welcoming("Frances Jane")
+welcoming("Nica Ella")
+welcoming("Leslie Boy")
+welcoming("Marianne Mae")
+welcoming("John Say")
---
+++ ACTIVITY24_1.py
@@ -0,0 +1,5 @@
+from ACTIVITY24 import welcoming, sum
+
+sum(24)
+
+welcoming("BSIT_1A")
